c76a17b8f717ed8a7e373d80f594c0a00430622a
jacknb <zhuningbin@sina.com>
2017-09-09 23:02:16 +0800

    eureka

--- src/main/java/com/adelmo/cloud/Application.java
+++ src/main/java/com/adelmo/cloud/Application.java
@@ -1,26 +1,18 @@
 package com.adelmo.cloud;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Created by znb on 17-9-8.
  */
 
-@Controller
+@EnableEurekaClient
 @SpringBootApplication
 public class Application {
 
-    @ResponseBody
-    @RequestMapping("/")
-    public String home() {
-        return "hello world!";
-    }
-
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        new SpringApplicationBuilder(Application.class).web(true).run(args);
     }
 }
